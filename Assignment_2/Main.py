7481f0a208fd106e06e8b879c3413109546b0741
YileChoi <choiyile@gmail.com>
2022-10-22 19:08:33 -0400

    Finished working on the project

--- Assignment_2/Main.py
+++ Assignment_2/Main.py
@@ -3,4 +3,40 @@ from Pizza import Pizza
 
 asdf = User("Yile", "test email", "test address")
 
-print(asdf.name)
+name = input("What is your name?: ")
+email = input("What is your email address?: ")
+address = input("What is your address?: ")
+user_info = User(name, email, address)
+
+size = ""
+pizzaNumber = 0
+price = 0
+
+user_pizza = Pizza(size, price, pizzaNumber)
+while True:
+  size = input("What size Pizza would you like? (Small/Medium/Large): ")
+  if size == "Small":
+    user_pizza.set_price(10)
+    user_pizza.set_size("Small")
+    break
+  elif size == "Medium":
+    user_pizza.set_price(12)
+    user_pizza.set_size("Medium")
+    break
+  elif size == "Large":
+    user_pizza.set_price(15)
+    user_pizza.set_size("Large")
+    break
+  else:
+    print("wrong input. Please enter again.")
+    size = input("What size Pizza would you like? (Small/Medium/Large): ")
+
+print("One Pizza of this size is $%.1f" %user_pizza.get_price())
+pizzaNumber = int(input("How many Pizzas of this size would you like?: "))
+user_pizza.set_pizzaNumber(pizzaNumber)
+
+print("You have ordered %i %s pizzas" %(user_pizza.get_pizzaNumber(), user_pizza.get_size()))
+
+print("Your total is, $ %.1f" %user_pizza.total())
+print("Order will be delivered to Tim at %s" %user_info.address)
+print("Receipt will be emailed to %s" %user_info.email)
--- Assignment_2/Pizza.py
+++ Assignment_2/Pizza.py
@@ -4,6 +4,27 @@ class Pizza:
     self.price = price
     self.pizzaNumber = pizzaNumber
   
-  @property
+  #getters
   def get_size(self):
     return self.size
+  def get_price(self):
+    return self.price
+  def get_pizzaNumber(self):
+    return self.pizzaNumber
+  
+  #setters
+  def set_size(self, pizzaSize):
+    if (pizzaSize == "Small") or (pizzaSize == "Medium") or (pizzaSize == "Large"):
+      self.size = pizzaSize
+    else:
+      return False
+  def set_price(self, pizzaPrice):
+    self.price = pizzaPrice
+  def set_pizzaNumber(self, number):
+    self.pizzaNumber = number
+
+  def total(self):
+    if (self.pizzaNumber < 3) and (self.pizzaNumber > 0):
+      return self.price * self.pizzaNumber
+    elif self.pizzaNumber >= 3:
+      return (self.price * self.pizzaNumber) - (self.price * self.pizzaNumber)*0.15
